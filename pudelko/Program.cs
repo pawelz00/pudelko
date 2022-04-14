49edbb8ca8b9d0c2e367ad97ff03fda29636e022
pawelz00 <pawet.wets@gmail.com>
2022-04-14 21:05:29 +0200

    add functionalities

--- pudelko/Program.cs
+++ pudelko/Program.cs
@@ -2,5 +2,10 @@
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-Pudelko pudelko = new Pudelko();
-Console.WriteLine(pudelko);
+Pudelko pudelko = new Pudelko(1,2.1,3.05,unit: Pudelko.UnitOfMeasure.meter);
+Pudelko pudelko2 = new Pudelko(2100, 1000, 3050, Pudelko.UnitOfMeasure.milimeter);
+Console.WriteLine(pudelko.Equals(pudelko2));
+var pudlo = new Pudelko();
+
+var pudelkooo = new Pudelko(a:4, c:4, unit:Pudelko.UnitOfMeasure.centimeter);
+Console.WriteLine(pudelkooo);
--- pudelko/Pudelko.cs
+++ pudelko/Pudelko.cs
@@ -7,47 +7,155 @@ using System.Threading.Tasks;
 
 namespace pudelko
 {
-    public enum UnitOfMeasure
+    public sealed class Pudelko : IFormattable, IEquatable<Pudelko>
     {
-        meter,
-        centimeter,
-        milimeter
-    }
-    public sealed class Pudelko
-    {
-        private readonly double a = 0.1, b = 0.1, c = 0.1;
-        public UnitOfMeasure unit { get; set; }
-        public double changeToMeters(UnitOfMeasure unit, double x)
+        #region Properties
+        private readonly double a, b, c;
+        public double A 
         {
-            if (unit == UnitOfMeasure.milimeter)
-                return Math.Round(x * 1000, 3);
-            else if (unit == UnitOfMeasure.centimeter)
-                return Math.Round(x * 100, 3);
-            else return Math.Round(x, 3);
+            get
+            {
+                double temporaryNumber = a;
+                if (unit == UnitOfMeasure.centimeter)
+                {
+                    temporaryNumber = a / 100;
+                }
+                else if (unit == UnitOfMeasure.milimeter)
+                {
+                    temporaryNumber = a / 1000;
+                }
+                return GetRoundedProperties(temporaryNumber);
+            }
         }
-        public double A { get => changeToMeters(unit, a); }
-        public double B { get => changeToMeters(unit, b); }
-        public double C { get => changeToMeters(unit, c); }
-        public Pudelko() => unit = UnitOfMeasure.meter;
-        public Pudelko(double a = 0.1, double b = 0.1, double c = 0.1, UnitOfMeasure unitOfMeasure = UnitOfMeasure.meter)
+        public double B
         {
-            if ((a < 0 || b < 0 || c < 0) || (a > 10 || b > 10 || c > 10))
+            get
+            {
+                double temporaryNumber = b;
+                if (unit == UnitOfMeasure.centimeter)
+                {
+                    temporaryNumber = b / 100;
+                }
+                else if (unit == UnitOfMeasure.milimeter)
+                {
+                    temporaryNumber = b / 1000;
+                }
+                return GetRoundedProperties(temporaryNumber);
+            }
+        }
+        public double C
+        {
+            get
+            {
+                double temporaryNumber = c;
+                if (unit == UnitOfMeasure.centimeter)
+                {
+                    temporaryNumber = c / 100;
+                }
+                else if (unit == UnitOfMeasure.milimeter)
+                {
+                    temporaryNumber = c / 1000;
+                }
+                return GetRoundedProperties(temporaryNumber);
+            }
+        }
+        public UnitOfMeasure unit { get; set; }
+        public double Objetosc { get => Math.Round(A * B * C, 9); }
+        public double Pole { get => Math.Round(2*((A*B)+(A*C)+(B*C)), 6); }
+        #endregion
+        #region Constructor
+        public Pudelko(double? a = null, double? b = null, double? c = null, UnitOfMeasure unit = UnitOfMeasure.meter)
+        {
+            this.unit = unit;
+            this.a = a.GetValueOrDefault(GetDefaultValue());
+            this.b = b.GetValueOrDefault(GetDefaultValue());
+            this.c = c.GetValueOrDefault(GetDefaultValue());
+            if (a < 0 || b < 0 || c < 0)
             {
                 throw new ArgumentOutOfRangeException();
             }
-            this.a = a; this.b = b; this.c = c;
+            if (A > 10 || B > 10 || C > 10)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
         }
+        #endregion
+        #region ToString
         public override string ToString()
         {
-            return $"{A.ToString("0.000", CultureInfo.InvariantCulture)} m × {B.ToString("0.000", CultureInfo.InvariantCulture)} m × {C.ToString("0.000", CultureInfo.InvariantCulture)} m";
+            if (unit == UnitOfMeasure.meter)
+                return $"{A.ToString("0.000", CultureInfo.InvariantCulture)} m \u00D7 {B.ToString("0.000", CultureInfo.InvariantCulture)} m \u00D7 {C.ToString("0.000", CultureInfo.InvariantCulture)} m";
+            else if (unit == UnitOfMeasure.centimeter)
+                return ToString("cm");
+            else return ToString("mm");
         }
-        //public override string ToString(string format)
-        //{
+        public string ToString(string format)
+        {
+            return ToString(format, CultureInfo.InvariantCulture);
+        }
+        public string ToString(string? format, IFormatProvider? formatProvider)
+        {
+            switch (format)
+            {
+                case "cm":
+                    return $"{String.Format(formatProvider,"{0:0.0}", A * 100)} cm \u00D7 {String.Format(formatProvider,"{0:0.0}", B * 100)} cm \u00D7 {String.Format(formatProvider,"{0:0.0}", C * 100)} cm";
+                case "mm":
+                    return $"{String.Format(formatProvider,"{0:0}", A * 1000)} mm \u00D7 {String.Format(formatProvider,"{0:0}", B * 1000)} mm \u00D7 {String.Format(formatProvider,"{0:0}", C * 1000)} mm";
+                case "m":
+                    return ToString();
+                default:
+                    throw new FormatException();
+            }
+        }
+        #endregion
+        #region Metody wyznaczania a,b,c
+        private double GetDefaultValue()
+        {
+            switch (unit)
+            {
+                case UnitOfMeasure.milimeter:
+                    return 100;
+                case UnitOfMeasure.centimeter:
+                    return 10;
+                default:
+                    return 0.1;
+            }
+        }
 
-        //}
-        //public string ToString(string? format, IFormatProvider? formatProvider)
-        //{
-        //    return a.ToString(format, formatProvider);
-        //}
+        public static double GetRoundedProperties(double x)
+        {
+           return Math.Round(x, 3);
+        }
+
+        #endregion
+        #region Enum
+        public enum UnitOfMeasure
+        {
+            meter,
+            centimeter,
+            milimeter
+        }
+        #endregion
+        #region Equals
+        public bool Equals(Pudelko? other)
+        {
+            if(ReferenceEquals(null, other)) return false;
+            return (this.Objetosc == other.Objetosc) && (this.Pole == other.Pole);
+        }
+        public override bool Equals(object? obj)
+        {
+            return this.Equals(obj);
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(A, B, C);
+        }
+        #endregion
+        public static bool operator ==(Pudelko p1, Pudelko p2) => p1.Equals(p2);
+        public static bool operator !=(Pudelko p1, Pudelko p2) => !p1.Equals(p2);
+        public static Pudelko operator +(Pudelko p1, Pudelko p2)
+        {
+
+        }
     }
 }
